b057e0cac15a367bebdf491c8d96f7707d07187f
Dan Chen <dyc5828@gmail.com>
2015-04-21 17:17:38 -0700

    initial commit

---
+++ data.js
@@ -0,0 +1,26 @@
+	$scope.pre = [{
+		first: 'Patric',
+		last: 'Dent',
+		org: 'USC',
+		email: 'dent@usc.edu',
+		phone: '1(321)555-1234'
+	},{
+		first: 'Bo',
+		last: 'Wang',
+		org: 'Aurfy',
+		email: 'yuanbo@usc.edu',
+		phone: '1(321)555-1234'
+	},{
+		first: 'Zune',
+		last: 'Nguyen',
+		org: 'Boz Way',
+		email: 'tridungn@usc.edu',
+		phone: '1(321)555-1234'
+	},{
+		first: 'Dan',
+		last: 'Chen',
+		org: 'USC',
+		email: 'dongyanc@usc.edu',
+		phone: '1(321)555-1234'
+	}];
+	$scope.contacts = $scope.pre;
